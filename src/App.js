bc8be77c378afe0561ed6916646cdd7a1b9f4254
andrew <statco19@gmail.com>
2022-07-16 17:16:10 +0900

    created an event handler that creates checkboxes for added todos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import TodoList from "./TodoList";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2']);
+  const [todos, setTodos] = useState([]);
+  const todoRefName = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoRefName.current.value;
+    if(name === "") return;
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}];
+    })
+    todoRefName.current.value = null;
+  }
+
   return (
     <>
     <TodoList todos={todos} />
-      <input type="text" />
+      <input ref={todoRefName} type="text" />
       <button>Clear</button>
-      <button>Add</button>
+      <button onClick={handleAddTodo}>Add</button>
       <div>0 left to do</div>
     </>
   )
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Todo from './Todo';
 
 export default function TodoList({ todos }) {
   return ( 
-    <div>{todos.length}</div>
+    todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} />
+    })
   )
 }
